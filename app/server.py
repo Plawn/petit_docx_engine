a20c63e5994ffb80bdca99a2f637d9b4119b3d1d
Paul Leveau <plawn.yay@juniorisep.com>
2020-04-09 19:11:10 +0200

    added /list + /remove_template for api-doc2 interface

--- app/server.py
+++ app/server.py
@@ -6,25 +6,39 @@ from .utils import download_minio_stream
 from typing import List, Dict
 import os
 import logging
+from dataclasses import dataclass
+import time
 
 minio_client: minio.Minio = None
 
-db: Dict[str, Template] = {}
-template_folder = 'templates'
 
-if not os.path.exists(template_folder):
-    os.mkdir(template_folder)
+@dataclass
+class TemplateContainer:
+    templater: Template
+    pulled_at: float
+
+
+# in memory database
+# rebuilded at each boot
+db: Dict[str, TemplateContainer] = {}
+
+TEMPLATE_FOLDER = 'templates'
+
+# ensure we can pull the data in the folder
+if not os.path.exists(TEMPLATE_FOLDER):
+    os.mkdir(TEMPLATE_FOLDER)
 
 
 app = Flask(__name__)
 
 
 def make_name(filename: str) -> str:
-    return os.path.join(template_folder, filename)
+    return os.path.join(TEMPLATE_FOLDER, filename)
 
 
 @app.route('/configure', methods=['POST'])
 def configure():
+    # maybe have a global conf object instead
     global minio_client
     data = request.get_json()
 
@@ -41,31 +55,34 @@ def configure():
     return jsonify({'error': False}), 200
 
 
+def pull_template(template_infos: dict):
+    remote_bucket = template_infos['bucket_name']
+    template_name = template_infos['template_name']
+    doc = minio_client.get_object(remote_bucket, template_name)
+    name = make_name(template_name)
+    download_minio_stream(doc, name)
+    db[template_name] = TemplateContainer(Template(name), time.time())
+
+
 @app.route('/load_templates', methods=['POST'])
 def load_template():
     data: List[dict] = request.get_json()
     success = []
     failed = []
-    for item in data:
+    for template_infos in data:
         try:
-            remote_bucket = item['bucket_name']
-            template_name = item['template_name']
-            doc = minio_client.get_object(remote_bucket, template_name)
-            name = make_name(template_name)
-            download_minio_stream(doc, name)
-            db[template_name] = Template(name)
-            success.append(item)
+            pull_template(template_infos)
+            success.append(template_infos)
         except:
             logging.error(traceback.format_exc())
-            traceback.print_exc()
-            failed.append(item)
+            failed.append(template_infos)
     return jsonify({'success': success, 'failed': failed})
 
 
 @app.route('/get_placeholders', methods=['POST'])
 def get_placeholders():
     data = request.get_json()
-    return jsonify(db[data['name']].fields)
+    return jsonify(db[data['name']].templater.fields)
 
 
 @app.route('/publipost', methods=['POST'])
@@ -75,15 +92,16 @@ def publipost():
     try:
 
         data: str = body['data']
-        name: str = body['template_name']
+        template_name: str = body['template_name']
         output_bucket: str = body['output_bucket']
         output_name: str = body['output_name']
         # don't actually know if will get used
         options = body.get('options', [])
         push_result = body.get('push_result', True)
-        output = db[name].render(data)
+        output = db[template_name].templater.render(data)
 
         if push_result:
+            # should make abstraction to push the result here
             minio_client.fput_object(output_bucket, output_name, output)
             return jsonify({'error': False})
         else:
@@ -91,12 +109,27 @@ def publipost():
             # should push the file back
             return jsonify({'result': 'OK'})
     except Exception as e:
-        traceback.print_exc()
+        logging.error(traceback.format_exc())
         return jsonify({'error': True}), 500
 
     finally:
         if output is not None:
             os.remove(output)
+
+
+@app.route('/list', methods=['GET'])
+def get_templates():
+    return jsonify({key: value.pulled_at for key, value in db.items()})
+
+@app.route('/remove_template', methods=['DELETE'])
+def remove_template():
+    js = request.get_json()
+    try :
+        template_name:str = js['template_name']
+        del db[template_name]
+        return jsonify({'error':False})
+    except:
+        return jsonify({'error':True}), 400
 
 
 @app.route('/live', methods=['GET'])
